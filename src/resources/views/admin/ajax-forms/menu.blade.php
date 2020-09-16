54e75a14a542a8ec7152def094e36a3b98a83ad2
Vladimir <vladimirpeshkov5dev@yandex.ru>
2020-09-16 11:58:55 +0300

    change bs version

--- src/resources/views/admin/ajax-forms/menu.blade.php
+++ src/resources/views/admin/ajax-forms/menu.blade.php
@@ -1,6 +1,11 @@
 @can("viewAny", \App\AjaxFormSubmission::class)
     @can('settings-management')
-        <li class="nav-item">
+        <li class="nav-item{{ in_array($currentRoute, [
+                            "admin.ajax-forms.index",
+                            "admin.ajax-forms.create",
+                            "admin.ajax-forms.show",
+                            "admin.ajax-forms.edit"
+                        ]) ? ' active' : '' }}">
             <a class="nav-link{{ in_array($currentRoute, [
                             "admin.ajax-forms.index",
                             "admin.ajax-forms.create",
@@ -15,7 +20,7 @@
             </a>
         </li>
     @endcan
-    <li class="nav-item">
+    <li class="nav-item {{ strstr($currentRoute, 'admin.ajax-forms.submissions.') !== FALSE ? ' active' : '' }}">
         <a href="{{ route("admin.ajax-forms.submissions.index") }}"
            class="nav-link{{ strstr($currentRoute, 'admin.ajax-forms.submissions.') !== FALSE ? ' active' : '' }}">
             @isset($ico)
