cf7703d5382ab238a9ef72851ce3a101037dd745
Nataliess <n.borisova@me.com>
2025-09-25 15:03:18 +0300

    policy input

---
+++ src/resources/views/site/includes/policy-input.blade.php
@@ -0,0 +1,20 @@
+<div class="custom-control custom-checkbox">
+    <input type="checkbox"
+           class="custom-control-input @error("privacy_policy") is-invalid @enderror"
+           id="privacy_policy{{ $postfix }}"
+           required
+           name="privacy_policy">
+    <label class="custom-control-label" for="privacy_policy{{ $postfix }}">
+        Я даю {{ config("policy.company", "") }} свое
+        @if (\Illuminate\Support\Facades\Route::has("policy"))
+            <a href="#agreementModal" data-bs-toggle="modal" data-bs-target="#agreementModal">Согласие на обработку персональных данных</a> и принимаю условия <a href="{{ route("policy") }}" target="_blank">Политики по обработке персональных данных</a>
+        @else
+            Cогласие на обработку персональных данных и принимаю условия Политики по обработке персональных данных
+        @endif
+    </label>
+    @error("privacy_policy")
+    <div class="invalid-feedback" role="alert">
+        {{ $message }}
+    </div>
+    @enderror
+</div>
